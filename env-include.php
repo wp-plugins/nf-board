2f209018ccb15d8a72a3d783e4b4d503c01e3b8a
nflint <nflint@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-09 04:03:19 +0000

    nfboard hold
    git-svn-id: https://plugins.svn.wordpress.org/nf-board/trunk@1063651 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- env-include.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once(plugin_dir_path(__FILE__)."inc/lib/NFB-Class.php");
-require_once(plugin_dir_path(__FILE__)."inc/lib/NFB-Func.php");
-?>
--- templates/member/Default/tf_errorbox.php
+++
@@ -1,3 +0,0 @@
-<div id="NFBoard_Content">
-	<div class="alert alert-warning text-center" role="alert"><?php echo $msg;?></div>
-</div>
